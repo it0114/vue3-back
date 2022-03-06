d18f2576cd3916687778442d3f5d7436f8f565bc
yanmiao <yanmiao@shinemo.com>
2022-03-06 17:50:15 +0800

    feat: element-push 按需导入

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-// https://vitejs.dev/config/
 export default defineConfig({
     server: {
         host:'127.0.0.1', // 启动时使用的路径 localhost
         port: '3000', // 指定端口号
         open:'/', // 启动打开页面
     },
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+    ],
 })
