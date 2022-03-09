e7e92f5689216ff90542c3fd9af143b0d6b2e3b7
yanmiao <yanmiao@shinemo.com>
2022-03-09 10:52:11 +0800

    feat : 配置 log4js 日志

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
     server: {
         host:'127.0.0.1', // 启动时使用的路径 localhost
-        port: '3000', // 指定端口号
+        port: '8080', // 指定端口号
         open:'/', // 启动打开页面
     },
     plugins: [
